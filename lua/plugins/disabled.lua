81a25395177c1fd6678f9008818aaf020063216a
Henry Chea <work@henrychea.com>
2024-01-29 23:41:08 +0700

    feat: update extras

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,3 +1,5 @@
 return {
-    { "folke/flash.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
+  -- { "rcarriga/nvim-notify", enabled = false },
+  -- { "folke/noice.nvim", enabled = false },
 }
--- lua/plugins/extra.lua
+++ lua/plugins/extra.lua
@@ -73,34 +73,8 @@ return {
   },
   {
     "nvim-lualine/lualine.nvim",
-    opts = {
-      options = {
-        theme = "auto",
-        component_separators = "|",
-        section_separators = { left = "", right = "" },
-      },
-      sections = {
-        lualine_a = {
-          { "mode", separator = { left = "" }, right_padding = 2 },
-        },
-        lualine_b = { "filename", "branch" },
-        lualine_c = { "fileformat" },
-        lualine_x = {},
-        lualine_y = { "filetype", "progress" },
-        lualine_z = {
-          { "location", separator = { right = "" }, left_padding = 2 },
-        },
-      },
-      inactive_sections = {
-        lualine_a = { "filename" },
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = { "location" },
-      },
-      tabline = {},
-      extensions = {},
-    },
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_x, 2, require("lazyvim.util").lualine.cmp_source("codeium"))
+    end,
   },
 }
