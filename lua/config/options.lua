67b7d8ad5a4b978bc67d3157f0b23ff6c34e50bf
Henry Chea <work@henrychea.com>
2024-04-09 18:09:28 +0700

    feat: lf in windows

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+-- Set shell to PowerShell 7 if on Win32 or Win64
+if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+  opt.fileformat = "unix"
+  opt.fileformats = "unix"
+end
