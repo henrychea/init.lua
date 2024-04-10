66c2a793e0a92204263783ce8c5d27b92965dbac
Henry Chea <work@henrychea.com>
2024-04-10 14:21:17 +0700

    feat: enable transparency

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,38 +4,59 @@ return {
     name = "catppuccin",
     priority = 1,
     lazy = false,
+    opts = {
+      transparent_background = true,
+    },
   },
   {
     "folke/tokyonight.nvim",
     opts = {
       theme = "night",
+      transparent = true,
     },
   },
   {
     "craftzdog/solarized-osaka.nvim",
-    opts = {},
+    opts = {
+      transparent = true,
+    },
   },
   {
     "navarasu/onedark.nvim",
     opts = {
       style = "darker",
+      transparent = true,
     },
   },
   {
     "Tsuzat/NeoSolarized.nvim",
+    opts = {
+      transparent = true,
+    },
   },
   {
     "loctvl842/monokai-pro.nvim",
+    opts = {
+      transparent_background = true,
+    },
   },
-  { "ellisonleao/gruvbox.nvim" },
   {
     "rebelot/kanagawa.nvim",
     opts = {
       theme = "dragon",
+      transparent = true,
     },
   },
-  { "projekt0n/github-nvim-theme" },
-  -- Configure LazyVim to load gruvbox
+  {
+    "projekt0n/github-nvim-theme",
+    config = function()
+      require("github-theme").setup({
+        options = {
+          transparent = true,
+        },
+      })
+    end,
+  },
   {
     "LazyVim/LazyVim",
   },
