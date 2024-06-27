94364ecce97bd430a13ba4945ca2f8eeca4d9361
Henry Chea <work@henrychea.com>
2024-06-27 22:28:03 +0700

    feat: update lsps

--- lua/plugins/extra.lua
+++ lua/plugins/extra.lua
@@ -1,52 +1,8 @@
--- In your plugin files, you can:
--- * add extra plugins
--- * disable/enabled LazyVim plugins
--- * override the configuration of LazyVim plugins
-
 return {
   -- Toggleterm
   { "akinsho/toggleterm.nvim", version = "*", config = true },
   -- Undo history
   { "mbbill/undotree" },
-  -- Configure LSP
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "stylua",
-        "biome",
-        "rust-analyzer",
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      -- make sure mason installs the server
-      servers = {
-        tsserver = {
-          enabled = false,
-        },
-        vtsls = {
-          -- explicitly add default filetypes, so that we can extend
-          -- them in related extras
-          settings = {
-            typescript = {
-              preferences = {
-                importModuleSpecifier = { enabled = "relative" },
-              },
-            },
-          },
-        },
-      },
-      setup = {
-        tsserver = function()
-          -- disable tsserver
-          return true
-        end,
-      },
-    },
-  },
   {
     "rcarriga/nvim-notify",
     opts = {
@@ -67,17 +23,16 @@ return {
     },
   },
   {
-    "nvim-cmp",
-    opts = function(_, opts)
-      table.insert(opts.sources, 2, {
-        name = "codeium",
-        group_index = 2,
-        priority = 100,
-      })
-    end,
-  },
-  {
     "xiyaowong/transparent.nvim",
     lazy = false,
+  },
+  {
+    "s1n7ax/nvim-window-picker",
+    name = "window-picker",
+    event = "VeryLazy",
+    version = "2.*",
+    config = function()
+      require("window-picker").setup()
+    end,
   },
 }
