4cfa7ba7745321ac9be929f7fd61f7596d067bbc
Henry Chea <work@henrychea.com>
2024-07-03 18:38:44 +0700

    fix: remove codeium

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,8 +10,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim",                                 import = "lazyvim.plugins" },
-    { import = "lazyvim.plugins.extras.coding.codeium" },
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.vue" },
     { import = "lazyvim.plugins.extras.lang.rust" },
