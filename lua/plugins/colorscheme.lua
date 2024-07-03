65d2360746ac09f31b5bf881f60e4df9bbca467d
Henry Chea <work@henrychea.com>
2024-07-03 16:44:02 +0700

    fix: catpuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,9 +4,6 @@ return {
   },
   {
     "navarasu/onedark.nvim",
-    opts = {
-      style = "darker",
-    },
   },
   {
     "Tsuzat/NeoSolarized.nvim",
@@ -17,12 +14,51 @@ return {
   { "rose-pine/neovim", name = "rose-pine" },
   {
     "rebelot/kanagawa.nvim",
-    opts = {
-      theme = "dragon",
-    },
   },
   {
     "projekt0n/github-nvim-theme",
+  },
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    opts = {
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
   },
   {
     "LazyVim/LazyVim",
