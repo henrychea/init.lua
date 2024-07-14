12846aa33741d3be77abbfc343cbd940dca6703a
Henry Chea <work@henrychea.com>
2024-07-14 14:03:04 +0700

    feat: add sessions

--- lua/henry/plugins/ui.lua
+++ lua/henry/plugins/ui.lua
@@ -78,13 +78,47 @@ return {
 		end,
 	},
 	{
+		"folke/persistence.nvim",
+		event = "BufReadPre", -- this will only start session saving when an actual file was opened
+		opts = {
+			-- add any custom options here
+		},
+	},
+	{
 		"nvimdev/dashboard-nvim",
-		event = "VimEnter",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		lazy = false,
 		config = function()
 			require("dashboard").setup({
-				-- config
+				theme = "hyper",
+				config = {
+					week_header = {
+						enable = true,
+					},
+					shortcut = {
+						{ desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
+						{
+							icon = " ",
+							icon_hl = "@variable",
+							desc = "Restore Session",
+							group = "Label",
+							action = 'lua require("persistence").load()',
+							key = "s",
+						},
+						{
+							icon = " ",
+							icon_hl = "@variable",
+							desc = "Files",
+							group = "Label",
+							action = "Telescope find_files",
+							key = "f",
+						},
+						{ desc = " Quit", group = "@property", action = "qa", key = "q" },
+					},
+				},
 			})
 		end,
-		dependencies = { { "nvim-tree/nvim-web-devicons" } },
 	},
 }
